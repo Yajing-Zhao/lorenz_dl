9806c1f3f5e0fc51047bb1e17fc5e77d62fa2954
yajing <yajing0130@gmail.com>
2019-02-13 22:21:31 -0700

    hidden

--- lorenz_rnn.py
+++ lorenz_rnn.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 import torch.optim as optim
 from mpl_toolkits.mplot3d import Axes3D
 INPUT_SIZE = 3
-HIDDEN_SIZE = 16
+HIDDEN_SIZE = 256
 OUTPUT_SIZE = 3
 LR = 0.01
 
@@ -18,7 +18,7 @@ para = (10.0, 28, 8.0/3.0)
 def f(state, t):
     x, y, z = state
     sigma, rho, beta = para
-    return sigma * (y - x), x * (rho - z - y), x * y - beta * z
+    return sigma * (y - x), x * (rho - z) - y, x * y - beta * z
 # time points
 t_seq = np.arange(0, 40, 0.01)
 
@@ -29,7 +29,7 @@ states = odeint(f, state0, t_seq)
 inputs = torch.FloatTensor(states[:-1]).view(1, states.shape[0] - 1, 3)
 targets = torch.FloatTensor(states[1:]).view(1, states.shape[0] - 1, 3)
 
-print(states.shape)
+print(states[:10])
 
 # Define the model
 class RnnNet(nn.Module):
@@ -59,7 +59,7 @@ hidden = Variable(torch.zeros(1, 1, HIDDEN_SIZE))
 inputs = Variable(inputs)
 targets = Variable(targets)
 
-for iter in range(1000):
+for iter in range(1001):
     output, hidden = model(inputs, hidden)
     hidden = hidden.detach()
 
@@ -86,12 +86,14 @@ print(predictions[:,0])
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
+print(predictions.shape)
 ax.plot(predictions[:, 0], predictions[:, 1], predictions[:, 2])
 plt.show()
 plt.savefig("out.png")
 
+print(states[:10])
 fig1 = plt.figure()
-ax =fig.gca(projection = '3d')
+ax =fig1.gca(projection = '3d')
 ax.plot(states[:,0], states[:,1], states[:,2])
 
 plt.savefig("out_exact.png")
